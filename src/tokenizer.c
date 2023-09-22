616cd0d7758345529d63c35141e520f987bd2f3e
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-09-23 01:17:10 +0700

    src:tokenizer.c: first working version

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -13,7 +13,33 @@ typedef enum error_code {
   E_INCORRECT_TOKEN,
 } ErrorCode;
 
+#define BAD_KEY -1
+typedef struct key_value_pair {
+  char* value;
+  int key;
+} KeyValuePair;
+
+int keyfromstring(char* value, const KeyValuePair lut[], int lut_size) {
+  for (int i = 0; i < lut_size; i++) {
+    KeyValuePair entry = lut[i];
+    if (strcmp(entry.value, value) == 0) {
+      return entry.key;
+    }
+  }
+  return BAD_KEY;
+}
+
+char* stringfromkey(int key, const KeyValuePair lut[], int lut_size) {
+  for (int i = 0; i < lut_size; i++) {
+    KeyValuePair entry = lut[i];
+    if (entry.key == key) {
+      return entry.value;
+    }
+  }
+  return NULL;
+}
 typedef enum token_type {
+  YET_UNDEFINED,
   LITERAL,
   VARIABLE,
   OPERATOR,
@@ -21,14 +47,68 @@ typedef enum token_type {
   FUNCTION,
   FUNCTION_ARGUMENT,
   FUNCTION_ARGUMENT_SEPARATOR,
-  PARANTHESIS_LEFT,
-  PARANTHESIS_RIGHT
+  PARENTHESIS_LEFT,
+  PARENTHESIS_RIGHT,
+  BAD_TOKEN
 } TokenType;
+const KeyValuePair TokenLUT[] = {
+    {"literal", LITERAL},
+    {"variable", VARIABLE},
+    {"operator", OPERATOR},
+    {"unary operator", UNARY_OPERATOR},
+    {"function", FUNCTION},
+    {"function argument", FUNCTION_ARGUMENT},
+    {"function argument separator", FUNCTION_ARGUMENT_SEPARATOR},
+    {"parenthesis left", PARENTHESIS_LEFT},
+    {"parenthesis right", PARENTHESIS_RIGHT},
+    {"bad token", BAD_TOKEN},
+};
+const int TokenLUT_size = sizeof(TokenLUT) / sizeof(TokenLUT[0]);
 
-typedef char* str;
+typedef enum operator{
+  ADDITION,
+  SUBTRACTION,
+  MULTIPLICATION,
+  DIVISION,
+  POWER,
+  MODULUS,
+} Operator;
+const KeyValuePair OperatorLUT[] = {
+    {"+", ADDITION}, {"-", SUBTRACTION}, {"*", MULTIPLICATION},
+    {"/", DIVISION}, {"^", POWER},       {"mod", MODULUS},
+};
+const int OperatorLUT_size = sizeof(OperatorLUT) / sizeof(OperatorLUT[0]);
+typedef enum function {
+  ARC_COSINE,
+  ARC_COTANGENT,
+  ARC_SINE,
+  ARC_TANENGT,
+  COSINE,
+  COTANGENT,
+  LOGARITHM_COMMON,
+  LOGARITHM_NATURAL,
+  SINE,
+  SQUARE_ROOT,
+  TANGENT,
+} Function;
+const KeyValuePair FunctionLUT[] = {
+    {"sin", SINE},
+    {"cos", COSINE},
+    {"tan", TANGENT},
+    {"cot", COTANGENT},
+    {"asin", ARC_SINE},
+    {"acos", ARC_COSINE},
+    {"atan", ARC_TANENGT},
+    {"acot", ARC_COTANGENT},
+    {"sqrt", SQUARE_ROOT},
+    {"ln", LOGARITHM_NATURAL},
+    {"log", LOGARITHM_COMMON},
+};
+const int FunctionLUT_size = sizeof(FunctionLUT) / sizeof(FunctionLUT[0]);
+
 typedef struct token {
   TokenType type;
-  str lexeme;
+  const char* lexeme;
 } Token;
 
 typedef struct token_node {
@@ -39,8 +119,8 @@ typedef struct token_node {
 int strcount(const char* str, char ch) {
   assert(str != NULL);
   int count = 0;
-  for (int i = 0; i < strlen(str); i++) {
-    if (isspace(str[i])) count++;
+  for (size_t i = 0; i < strlen(str); i++) {
+    if (str[i] == ch) count++;
   }
   return count;
 }
@@ -50,15 +130,15 @@ char* remove_spaces(const char* src) {
   int spaces_count = strcount(src, ' ');
   int res_len = strlen(src) - spaces_count + 1;
   char* res = calloc(res_len, sizeof(char));
-  *res = *src;
+  char* iter = res;
   while (*src != '\0') {
     if (*src != ' ') {
-      *res = *src;
-      res++;
+      *iter = *src;
+      iter++;
     }
     src++;
   }
-  *res = '\0';
+  *iter = '\0';
   return res;
 }
 
@@ -67,54 +147,76 @@ bool isparen(char ch) { return ch == '(' || ch == ')'; }
 bool isradixp(char ch) { return ch == '.'; }
 bool isdigitf(char ch) { return isdigit(ch) || isradixp(ch); }
 
-char* read_lexeme(const char* src, int* pos) {
+char* read_lexeme(const char* src, size_t* pos) {
   assert(src != NULL);
   assert(pos != NULL);
 
   int start_idx = *pos;
-  const char* ch = &src[*pos];
+  int end_idx = start_idx + 1;
 
-  int lexeme_len = 1;
   if (isdigit(src[start_idx])) {
     int radixp_count = 0;
-    while (isdigitf(*ch + lexeme_len)) {
-      if (isradixp(*ch + lexeme_len)) radixp_count++;
+    while (isdigitf(src[end_idx])) {
+      if (isradixp(src[end_idx])) radixp_count++;
       if (radixp_count == 2) break;
-      lexeme_len++;
+      end_idx++;
     }
   }
   if (isalpha(src[start_idx])) {
-    while (isalpha(*ch + lexeme_len)) {
-      lexeme_len++;
+    while (isalpha(src[end_idx])) {
+      end_idx++;
     }
   }
 
+  int lexeme_len = end_idx - start_idx;
   char* lexeme = calloc(lexeme_len + 1, sizeof(char));
   assert(lexeme != NULL);
-  strncpy(&src[*pos], lexeme, lexeme_len);
+  strncpy(lexeme, &src[*pos], lexeme_len);
   *pos += lexeme_len;
   return lexeme;
 }
 
-TokenType match_token_type(str lexeme) {
-  ;
-  ;
+bool l_isvariab(char* lexeme) { return isalpha(lexeme[0]); }
+bool l_isparenl(char* lexeme) { return lexeme[0] == '('; }
+bool l_isparenr(char* lexeme) { return lexeme[0] == ')'; }
+bool l_isfloat_wo_integral_part(char* lexeme) {
+  return isradixp(lexeme[0]) && strlen(lexeme) > 1;
+}
+bool l_isnumber(char* lexeme) {
+  return isdigit(lexeme[0]) || l_isfloat_wo_integral_part(lexeme);
+}
+bool l_issepart(char* lexeme) {
+  return lexeme[0] == ',' && strlen(lexeme) == 1;
+}
+bool l_isfunctn(char* lexeme) {
+  int key = keyfromstring(lexeme, FunctionLUT, FunctionLUT_size);
+  return key != BAD_KEY;
+}
+bool l_isoperat(char* lexeme) {
+  int key = keyfromstring(lexeme, OperatorLUT, OperatorLUT_size);
+  return key != BAD_KEY;
 }
 
-Token create_token(str lexeme, TokenType type) {
-  Token token;
-  token.lexeme = lexeme;
-  token.type = type;
-  return token;
+TokenType match_token_type(char* lexeme) {
+  if (l_isparenl(lexeme)) return PARENTHESIS_LEFT;
+  if (l_isparenr(lexeme)) return PARENTHESIS_RIGHT;
+  if (l_isnumber(lexeme)) return LITERAL;
+  if (l_issepart(lexeme)) return FUNCTION_ARGUMENT_SEPARATOR;
+  if (l_isfunctn(lexeme)) return FUNCTION;
+  if (l_isoperat(lexeme)) return OPERATOR;
+  if (l_isvariab(lexeme)) return VARIABLE;
+  return BAD_TOKEN;
 }
 
-Token read_token(const char* src, int* pos) {
+Token create_token(const char* lexeme, TokenType type) {
+  return (Token){type, lexeme};
+}
+Token read_token(const char* src, size_t* pos) {
   char* lexeme = read_lexeme(src, pos);
   TokenType type = match_token_type(lexeme);
   Token token = create_token(lexeme, type);
   return token;
 }
-
 TokenNode* create_token_node(Token token) {
   TokenNode* node = malloc(sizeof(TokenNode));
   node->token = token;
@@ -122,30 +224,40 @@ TokenNode* create_token_node(Token token) {
   return node;
 }
 
-TokenNode* tokenize(char* const src) {
+bool strisempty(const char* str) { return strlen(str) == 0; }
+
+TokenNode* tokenize(const char* src) {
   assert(src != NULL);
-  char* src_wo_spaces = remove_spaces(src);
-  if (strlen(src_wo_spaces) == 0) {
+  src = remove_spaces(src);
+  if (strisempty(src)) {
     return NULL;
   }
-  int pos = 0;
+  size_t pos = 0;
   Token token = read_token(src, &pos);
-  TokenNode* token_node = create_token_node(token);
-  while (token_node != NULL) {
+  TokenNode* list_head = create_token_node(token);
+  TokenNode* token_node = list_head;
+  while (pos < strlen(src)) {
     token = read_token(src, &pos);
     token_node->next = create_token_node(token);
     token_node = token_node->next;
   }
-  return token_node;
+  return list_head;
+}
+
+void print_tokens(const char* const src, const TokenNode* const list) {
+  const TokenNode* node = list;
+  printf("Source string: \"%s\"\n", src);
+  while (node) {
+    Token token = node->token;
+    char* type = stringfromkey(token.type, TokenLUT, TokenLUT_size);
+    printf("Type: \"%s\", Value: \"%s\"\n", type, token.lexeme);
+    node = node->next;
+  }
 }
 
 int main(void) {
-  char expr[] = "1+2-(3*8)";
+  char expr[] = "1.21. + 2*cos(2x)";
 
   TokenNode* token_list = tokenize(expr);
-  TokenNode* token_node = token_list;
-  while (token_node) {
-    printf("%s\n", token_node->token.lexeme);
-    token_node = token_node->next;
-  }
+  print_tokens(expr, token_list);
 }
