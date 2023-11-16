545da560e12bd3fa330e285ff9932a7421747a84
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-11-17 06:57:32 +0700

    src:lib:test: rename testfile for express testing from main to testf

---
+++ test.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#include "smartcalc.h"
+#define INPUT_LEN 256
+
+// add check for function arg separator validity
+int main(void) {
+  // char expr[] = "2 / (3 + 2) * 5";
+  // char expr[] = "-1";
+  // char expr[] = "(-1)";
+  // char expr[] = "1 + -2";
+  // char expr[] = "1 - -2";
+
+  // smartcalc_expr_analyze(expr);
+  // char* expr_postfix = smartcalc_expr_infix_to_postfix(expr);
+  // printf("%s\n", expr_postfix);
+  // double res = smartcalc_expr_infix_evaluate(expr);
+  // printf("%lf\n", res);
+
+  const char* expr = "2+1,3";
+
+  double res;
+  SmartCalcError rc = smartcalc_expr_infix_evaluate(expr, 0, &res);
+}
