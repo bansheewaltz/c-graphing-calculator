08a957a6659ab86eb02226364ab44f95b0c218f6
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-11-19 03:09:11 +0700

    format all the code with a clang-format

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -2,10 +2,9 @@
 
 #include "mainwindow.hpp"
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+int main(int argc, char *argv[]) {
+  QApplication a(argc, argv);
+  MainWindow w;
+  w.show();
+  return a.exec();
 }
