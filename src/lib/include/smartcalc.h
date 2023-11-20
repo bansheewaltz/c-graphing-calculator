a3b0ab2287dd4ad75d1f00d4afef425959a86202
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-11-20 19:22:22 +0700

    src:lib:include: document the functions in a doxygen format

--- src/lib/include/smartcalc.h
+++ src/lib/include/smartcalc.h
@@ -91,16 +91,84 @@ DepositOperation smartcalc_deposit_stackpop(DepositStack* stack);
 bool smartcalc_deposit_stackisfull(DepositStack* stack);
 bool smartcalc_deposit_stackisempty(DepositStack* stack);
 
+/**
+ * The function calculates the total interest, total tax, and end amount for a
+ * deposit based on various parameters.
+ *
+ * @param dstack The parameter `dstack` is of type `DepositStack` and represents
+ * a stack of deposit operations. It is used to keep track of the deposits made
+ * at different months during the period.
+ * @param depo_amount The deposit amount (initial amount of money deposited)
+ * @param period The "period" parameter represents the duration of the deposit
+ * in months.
+ * @param i_rate The parameter `i_rate` represents the annual interest rate for
+ * the deposit.
+ * @param t_rate The parameter `t_rate` represents the tax rate, which is the
+ * percentage of the interest that is deducted as tax.
+ * @param terms The "terms" parameter in the smartcalc_deposit_calculate
+ * function represents the type of deposit term. It can have three possible
+ * values:
+ * @param cap The parameter "cap" in the smartcalc_deposit_calculate function
+ * represents the capitalization frequency of the deposit. It determines how
+ * often the interest is added to the deposit amount.
+ * @param total_interest A pointer to a double variable that will store the
+ * total interest earned over the deposit period.
+ * @param total_tax A pointer to a double variable that will store the total tax
+ * amount calculated by the function.
+ * @param end_amount A pointer to a double variable that will store the final
+ * amount after the calculation.
+ *
+ * @return a value of type SmartCalcError.
+ */
 SmartCalcError smartcalc_deposit_calculate(
     DepositStack dstack, double depo_amount, double period, double i_rate,
     double t_rate, int terms, int cap, double* total_interest,
     double* total_tax, double* end_amount);
 
+/**
+ * The function calculates the fixed monthly payment, total interest expense,
+ * and total payment for a loan based on the loan amount, period, and interest
+ * rate.
+ *
+ * @param loan_amount The loan amount is the total amount of money borrowed.
+ * @param period The "period" parameter represents the number of months over
+ * which the loan will be repaid.
+ * @param interest_rate The interest rate is the annual interest rate for the
+ * loan.
+ * @param pay_mnth The monthly payment amount for the fixed credit.
+ * @param int_exp The parameter `int_exp` represents the total interest expense
+ * over the loan period.
+ * @param pay_tot The parameter `pay_tot` represents the total amount to be paid
+ * over the loan period, including both the principal amount and the interest.
+ *
+ * @return a value of type SmartCalcError.
+ */
 SmartCalcError smartcalc_credit_fixed_payment(double loan_amount, double period,
                                               double interest_rate,
                                               double* pay_mnth, double* int_exp,
                                               double* pay_tot);
 
+/**
+ * The function calculates the fixed payment and interest payment for a credit
+ * with a given loan amount, period, interest rate, and initial payment.
+ *
+ * @param loan_amount The loan amount is the total amount of money borrowed.
+ * @param period The period parameter represents the number of months over which
+ * the loan will be repaid.
+ * @param i The parameter "i" in the function smartcalc_credit_diff_payment
+ * represents the current period of the loan. It is used to calculate the
+ * current debt by subtracting the fixed payment multiplied by (i - 1) from the
+ * loan amount.
+ * @param interest_rate The `interest_rate` parameter represents the annual
+ * interest rate for the loan.
+ * @param fixed_payment The `fixed_payment` parameter is a pointer to a double
+ * variable that will store the calculated fixed payment amount.
+ * @param interest_payment The parameter `interest_payment` is a pointer to a
+ * double variable. It is used to store the calculated interest payment for the
+ * given loan amount, period, interest rate, and fixed payment.
+ *
+ * @return a value of type SmartCalcError.
+ */
 SmartCalcError smartcalc_credit_diff_payment(double loan_amount, double period,
                                              double i, double interest_rate,
                                              double* fixed_payment,
